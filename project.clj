48d0ac6abb60e49dd7ab347ebd54fc336adc740d
Mostafa Gomaa <mostafa.gomaa89@gmail.com>
2014-10-30 14:10:17 +0200

    Edits the description and url keys in the project.clj file

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject franco "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "This is a simple library that
+translates text written in Franco-Arabic into Arabic.
+The translation is implemeted as a finite state transducer."
+    :url "https://github.com/m-combinator/Franco/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
